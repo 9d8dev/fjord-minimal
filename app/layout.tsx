e262e2fba4d7c40ce9dfa8b5ad6d1d9763b7f1eb
Bridger Tower <bridgertower@gmail.com>
2024-03-25 11:21:11 -0600

    move nav and footer to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,27 @@
 import type { Metadata } from "next";
 import { Manrope, Instrument_Serif } from "next/font/google";
 import * as Craft from "@/components/craft/layout";
-import Nav from "@/components/craft/section/nav";
-import Footer from "@/components/craft/section/footer";
 import "./globals.css";
 import fjord from "@/fjord.config";
+
+// Component Imports
+import { ModeToggle } from "@/components/craft/theme/theme-toggle";
+import { Button } from "@/components/ui/button";
+import { NavMenu } from "@/components/craft/section/nav-menu";
+import { MobileNav } from "@/components/craft/section/mobile-nav";
+
+// React and Next Imports
+import Image from "next/image";
+import Link from "next/link";
+
+// Utility Imports
+import Logo from "@/public/logo.svg";
+import Balancer from "react-wrap-balancer";
+import { Github, Twitter } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+const github_link = "https://github.com/9d8dev/fjord-stripped";
+const x_link = "https://x.com/bridgertower";
 
 const manrope = Manrope({
   subsets: ["latin"],
@@ -48,3 +65,103 @@ export default function RootLayout({
     </Craft.Layout>
   );
 }
+
+const Nav = ({ className, children, id }: NavProps) => {
+  return (
+    <nav
+      className={cn(
+        "sticky z-50 top-0 bg-background",
+        "border-b drop-shadow-sm",
+        className
+      )}
+      id={id}
+    >
+      <div
+        id="nav-container"
+        className="max-w-5xl mx-auto py-4 px-6 sm:px-8 flex justify-between items-center"
+      >
+        <Link className="hover:opacity-75 transition-all" href="/">
+          <h2 className="sr-only">Craft UI</h2>
+          <Image src={fjord.logo} alt="Logo" width={72} height={48}></Image>
+        </Link>
+        {children}
+        <div className="flex items-center gap-2">
+          <NavMenu />
+          <Button asChild>
+            <Link href={fjord.menu.cta}>Get Started</Link>
+          </Button>
+          <MobileNav />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer className="border-t drop-shadow-sm">
+      <Craft.Section>
+        <Craft.Container className="grid gap-6 md:grid-cols-2">
+          <div className="grid gap-6">
+            <Link href="/">
+              <h3 className="sr-only">Craft UI</h3>
+              <Image src={Logo} alt="Logo" width={100} height={50}></Image>
+            </Link>
+            <p>
+              <Balancer>{fjord.site_description}</Balancer>
+            </p>
+
+            <div className="flex gap-2">
+              <ModeToggle />
+              <Button variant="outline" asChild size="icon">
+                <Link href={github_link}>
+                  <Github className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
+                  <span className="sr-only">Visit Github</span>
+                </Link>
+              </Button>
+              <Button variant="outline" asChild size="icon">
+                <Link href={x_link}>
+                  <Twitter className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
+                  <span className="sr-only">Visit Twitter</span>
+                </Link>
+              </Button>
+            </div>
+            <p className="text-muted-foreground hidden sm:block">
+              Code © <a href="https://9d8.dev">9d8</a>. 2023-present.
+            </p>
+          </div>
+
+          <div className="grid md:grid-cols-2 gap-6 sm:gap-0">
+            <div className="flex flex-col gap-2">
+              <h4 className="text-muted-foreground">{fjord.site_name}</h4>
+              <ul>
+                {Object.entries(fjord.menu.main).map(([key, href]) => (
+                  <li key={key}>
+                    <Link href={href}>
+                      {key.charAt(0).toUpperCase() + key.slice(1)}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="flex flex-col gap-2">
+              <h4 className="text-muted-foreground">Blog</h4>
+              <ul>
+                {Object.entries(fjord.menu.content).map(([key, href]) => (
+                  <li key={key}>
+                    <Link href={href}>
+                      {key.charAt(0).toUpperCase() + key.slice(1)}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <p className="text-muted-foreground block sm:hidden">
+            Code © <a href="https://9d8.dev">9d8</a>. 2023-present.
+          </p>
+        </Craft.Container>
+      </Craft.Section>
+    </footer>
+  );
+};
