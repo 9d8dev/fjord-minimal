a80f54470a04d47ace0d3694c6b7266403782a51
Bridger Tower <bridgertower@gmail.com>
2024-04-26 11:30:05 -0600

    Updated

--- components/global/recent-posts.tsx
+++ components/global/recent-posts.tsx
@@ -19,15 +19,17 @@ export default async function RecentPosts({
 
   return (
     <Section className="border-t prose-headings:m-0">
-      <Container>
-        <div className="flex flex-col gap-6">
-          <h2>Latest posts from {fjord.site_name}</h2>
-          <h4>Read the latest from our blog.</h4>
-          <div className="m-auto grid max-w-6xl gap-6 sm:grid-cols-2 md:grid-cols-3 mt-6 md:mt-12">
-            {filteredPosts.map((post: PostProps) => (
-              <PostCard key={post.id} post={post} />
-            ))}
-          </div>
+      <Container className="flex flex-col gap-6">
+        <h2>
+          Catch up on the latest guides and resources from {fjord.site_name}
+        </h2>
+        <h4 className="text-muted-foreground">
+          Read the latest from our blog.
+        </h4>
+        <div className="m-auto grid max-w-6xl gap-6 sm:grid-cols-2 md:grid-cols-3 mt-6 md:mt-12">
+          {filteredPosts.map((post: PostProps) => (
+            <PostCard key={post.id} post={post} />
+          ))}
         </div>
       </Container>
     </Section>
